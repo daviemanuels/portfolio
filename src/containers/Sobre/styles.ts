a90d67faf48f18879c845697c202b399d7cc1041
Davi Emanuel <daviiemanuel2000@gmail.com>
2023-08-21 08:06:37 -0300

    alterando os styles

--- src/containers/Sobre/styles.ts
+++ src/containers/Sobre/styles.ts
@@ -9,6 +9,11 @@ export const GithubSecao = styled.div`
 
   img {
     height: 157px;
+
+    @media (max-width: 768px) {
+      height: auto;
+      width: 100%;
+    }
   }
 
   @media (max-width: 768px) {
